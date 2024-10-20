e7b6285a1f4f02af1164afadf3a6b3ecb6d7810e
Jiazhen Xie <jiazhen@canva.com>
2024-10-20 15:34:37 +0100

    feat: add test for simulator

--- test/lib/montecarlo/simulator_test.rb
+++ test/lib/montecarlo/simulator_test.rb
@@ -2,14 +2,18 @@ require 'minitest/autorun'
 require 'montecarlo'
 
 class TestSimulator < Minitest::Test
-  @dice_picker = MonteCarlo::HistogramRandomPicker.new({ 1 => 1, 2 => 1, 3 => 1, 4 => 1, 5 => 1, 6 => 1 })
+  def setup
+    @dice_picker = MonteCarlo::HistogramRandomPicker.new({ 1 => 1, 2 => 1, 3 => 1, 4 => 1, 5 => 1, 6 => 1 })
+  end
   
   def test_simulator_with_dice_picker
     simulator = MonteCarlo::Simulator.new(@dice_picker)
     trials = 100_000
     results = simulator.run(trials)
     assert_equal trials, results.values.sum
-    assert_in_delta 1.0/6, results[1]/trials.to_f, 0.01
+    6.times do |i|
+      assert_in_delta 1.0/6, results[i+1]/trials.to_f, 0.01
+    end
   end
 end
 
